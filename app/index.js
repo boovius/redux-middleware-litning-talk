da09aa2c671c24803eb0457e1f4bd6742f360c5a
Joshua Book <joshua.book@gmail.com>
2016-05-24 10:17:23 -0400

    setup

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './stylesheets/normalize.scss';
+import './stylesheets/app.scss';
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div id='app'>
+        <h1>react webpack babel es2015 mocha hmre boilerplate</h1>
+        <p>by booivus</p>
+      </div>
+    )
+  }
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client',
+    './index',
+  ],
+
+  output: {
+    path: path.join(__dirname, '/bundle'),
+    publicPath: '/static/',
+    filename: 'index.js',
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      },
+      {
+        test: /\.html$/,
+        loaders: ['file?name=[name].[ext]']
+      }
+    ]
+  }
+};
