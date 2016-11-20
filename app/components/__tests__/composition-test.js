8a6349812bb40b1b603fcd58801eae899fdb05b1
Joshua Book <joshua.book@gmail.com>
2016-11-20 16:44:41 -0500

    composition component

---
+++ app/components/__tests__/composition-test.js
@@ -0,0 +1,91 @@
+import Composition from '../composition';
+
+describe('Composition', ()=>{
+  let composition, action;
+  const textInput = 'text-input';
+
+  beforeEach(()=>{
+    action = sinon.spy();
+
+    composition = mount(<Composition action={action} />);
+  });
+
+  describe('Initialization', ()=>{
+    it('has an action', ()=>{
+      expect(composition.prop('action')).to.eql(action);
+    });
+
+    it('has empty text', ()=>{
+      expect(composition.state('text')).to.eql('');
+    });
+  });
+
+  describe('Layout', ()=>{
+    it('has a text input', ()=>{
+      const input = composition.find('input');
+      expect(input.length).to.eql(1);
+    });
+
+    it('text input set to text on state', ()=>{
+      composition.setState({text: textInput});
+
+      const input = composition.find('input');
+      expect(input.prop('value')).to.eql(textInput);
+    });
+
+    it('has a send button', ()=>{
+      const button = composition.find('button');
+      expect(button.length).to.eql(1);
+    });
+  });
+
+  describe('Interaction', ()=>{
+    describe('changing text', ()=>{
+      it('updates text on state', ()=>{
+        const input = composition.find('input');
+        input.simulate('change', {target: {value: textInput}});
+        expect(composition.state('text')).to.eql(textInput);
+      });
+    });
+
+    describe('send', ()=>{
+      context('given text has value', ()=>{
+        it('sends text on state to action',()=>{
+          composition.setState({text: textInput});
+
+          const button = composition.find('button');
+          button.simulate('click');
+
+          expect(action.called).to.eql(true)
+          expect(action.lastCall.args[0]).to.eql(textInput);
+        });
+      });
+
+      context('given text is empty', ()=>{
+        const textInput = '';
+
+        it('sends text on state to action',()=>{
+          composition.setState({text: textInput});
+
+          const button = composition.find('button');
+          button.simulate('click');
+
+          expect(action.called).to.eql(false)
+        });
+      });
+
+      context('given text is null', ()=>{
+        const textInput = '';
+
+        it('sends text on state to action',()=>{
+          composition.setState({text: textInput});
+
+          const button = composition.find('button');
+          button.simulate('click');
+
+          expect(action.called).to.eql(false)
+        });
+      });
+    });
+  });
+});
---
+++ app/components/composition.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class Composition extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    };
+    this._updateText = this._updateText.bind(this);
+    this._sendAction = this._sendAction.bind(this);
+  }
+
+  _updateText(e) {
+    const text = e.target.value;
+    this.setState({text});
+  }
+
+  _sendAction() {
+    if (this.state.text === null || this.state.text === '') {
+      return
+    }
+
+    this.props.action(this.state.text);
+  }
+
+  render() {
+    return(
+      <div>
+        <input
+          type='text'
+          value={this.state.text}
+          onChange={this._updateText}
+        />
+        <button onClick={this._sendAction}>
+          send
+        </button>
+      </div>
+    )
+  }
+}
