1d37890e7c4068b309db2bcf99f27a720bffbf5a
Joshua Book <joshua.book@gmail.com>
2016-11-20 16:44:49 -0500

    chat component

--- app/components/__tests__/chat-test.js
+++ app/components/__tests__/chat-test.js
@@ -1,23 +1,54 @@
 import Chat from '../chat';
+import Conversation from '../conversation';
+import Composition from '../composition';
 
 describe('Chat', ()=> {
-  let chat;
+  let chat, messageData;
+  const textInput = 'text-input';
+
   beforeEach(() => {
-    chat = shallow(<Chat/>);
+    messageData = [
+      {
+        sender: 'sender-1',
+        content: 'message-1'
+      },
+      {
+        sender: 'sender-2',
+        content: 'message-2'
+      }
+    ]
+
+    chat = mount(<Chat messages={messageData} />);
   });
 
   describe('Initialization', ()=> {
+    it('sets messages on state to props passed in', ()=>{
+      expect(chat.state('messages')).to.eql(messageData)
+    });
   });
+
   describe('Layout', ()=> {
     it('has a conversation box', ()=> {
-      const conversation = chat.find('.converation');
+      const conversation = chat.find(Conversation);
       expect(conversation.length).to.eql(1);
+      expect(conversation.prop('messages')).to.eql(chat.state('messages'));
     });
-    it('has a message box', ()=> {
-      const message = chat.find('.message');
-      expect(message.length).to.eql(1);
+
+    it('has a composition box', ()=> {
+      const composition = chat.find(Composition);
+      expect(composition.length).to.eql(1);
     });
   });
+
   describe('Interaction', ()=> {
+    describe('adding new message', ()=>{
+      it('adds message to state', () => {
+        const composition = chat.find(Composition);
+        composition.prop('action')(textInput);
+
+        expect(chat.state('messages').length).to.eql(3);
+        expect(chat.state('messages').pop().content).to.eql(textInput);
+      });
+    });
   });
 });
--- app/components/chat.jsx
+++ app/components/chat.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Conversation from './conversation';
+import Composition from './composition';
 
 export default class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addMessage = this.addMessage.bind(this);
+    this.state = {
+      messages: this.props.messages
+    };
+  }
+
+  addMessage(text) {
+    let messages = this.state.messages;
+    messages.push({sender: '', content: text});
+    this.setState({messages});
+  }
+
   render(){
     return (
       <div>
-        <div className='conversation'></div>
-        <div className='message'></div>
+        <Conversation messages={this.state.messages} />
+        <Composition action={this.addMessage} />
       </div>
     )
   }
