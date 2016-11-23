9071cdc4ebb2020091709a445a2cd0889d59477a
Joshua Book <joshua.book@gmail.com>
2016-11-23 10:40:10 -0500

    get initial data from firebase

--- app/index.js
+++ app/index.js
@@ -5,34 +5,12 @@ import './stylesheets/app.scss';
 
 import Chat from './components/chat';
 
-const messages = [
-  {
-    sender: '1',
-    content: 'hello'
-  },
-  {
-    sender: '2',
-    content: 'goodbye'
-  },
-  {
-    sender: '1',
-    content: 'hello again'
-  },
-  {
-    sender: '2',
-    content: 'goodbye again'
-  },
-  {
-    sender: '1',
-    content: 'oh come on'
-  },
-]
 
 export default class App extends React.Component {
   render () {
     return (
       <div id='app'>
-        <Chat author='1' messages={messages}/>
+        <Chat author='1' messages={this.props.messages}/>
       </div>
     )
   }
