63f95dc89f008b912e81c2fa17bbc10ffa900d7a
Joshua Book <joshua.book@gmail.com>
2017-01-17 15:21:46 -0500

    basic styling and build out of navbar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -4,10 +4,17 @@ import { Link } from 'react-router';
 export default class Navbar extends Component {
   render () {
     return (
-      <div className='navbar'>
-        navbar
-        <Link to='login'>Login</Link>
-        <Link to='/'>Chat</Link>
+      <div id='navbar'>
+        <div className='home'>
+          navbar
+        </div>
+        <div className='dropdown'>
+          menu
+          <ul className='dropdown-content'>
+            <li><Link to='login'>Login</Link></li>
+            <li><Link to='/'>Chat</Link></li>
+          </ul>
+        </div>
       </div>
     )
   }
