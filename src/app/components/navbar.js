47aec2c49d3a8143d87ba0d0e1ba08de182247c6
Joshua Book <joshua.book@gmail.com>
2017-01-18 18:10:32 -0500

    make navbar look better

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 export default class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {open: false};
+    this._toggleMenu = this._toggleMenu.bind(this);
+  }
+
+  _toggleMenu() {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+  _displayDropdown() {
+    return this.state.open ? 'block' : 'none';
+  }
+
   render () {
     return (
       <div id='navbar'>
         <div className='home'>
-          navbar
+          <Link to='/'>Chat</Link>
         </div>
-        <div className='dropdown'>
-          menu
-          <ul className='dropdown-content'>
-            <li><Link to='login'>Login</Link></li>
-            <li><Link to='/'>Chat</Link></li>
+        <div onClick={this._toggleMenu} className='dropdown'>
+          <div className='dropdown-title'>menu</div>
+          <ul style={{display: this._displayDropdown()}} className='dropdown-content'>
+            <li className='link'><Link to='login'>Login</Link></li>
+            <li className='link'><Link to='/'>ChatAAAAAAAAAAAAAAAAAAAAA</Link></li>
           </ul>
         </div>
       </div>
