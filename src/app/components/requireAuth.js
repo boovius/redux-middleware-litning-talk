9f250f15a73cff93318c27a17e878deb24659dfd
Joshua Book <joshua.book@gmail.com>
2017-01-18 19:06:25 -0500

    requireAuth HOC wraps Chat component

---
+++ src/app/components/requireAuth.js
@@ -0,0 +1,40 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+export default (ComposedComponent) => {
+  class Authenticater extends Component {
+    componentWillMount() {
+      console.log('mounting authenticated');
+
+      if (!this.props.isAuthenticated) {
+        console.log('not authenticated');
+
+        this.context.router.push('login')
+      }
+    }
+
+    render() {
+      return <ComposedComponent />;
+    }
+
+  }
+
+  Authenticater.contextTypes = {
+    router: PropTypes.object.isRequired
+  }
+
+  function mapStateToProps(state) {
+    return {
+      isAuthenticated: state.auth.isAuthenticated
+    }
+  }
+
+  function mapDispatchToProps() {
+    return {}
+  }
+
+  return connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Authenticater);
+}
--- src/app/routes.js
+++ src/app/routes.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './index';
-import Chat from './components/chat';
 import Login from './components/login';
+import requireAuth from './components/requireAuth';
+import Chat from './components/chat';
 
 export default (
   <Route path='/' component={App} >
-    <IndexRoute component={Chat} />
+    <IndexRoute component={requireAuth(Chat)} />
     <Route path='login' component={Login} />
   </Route>
 )
