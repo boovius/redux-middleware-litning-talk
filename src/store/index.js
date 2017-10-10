5f558334270a7764aa472b96ab33199f5c728375
Joshua Book <joshua.book@gmail.com>
2017-10-10 18:56:48 -0400

    revert middlewares

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,7 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import rootReducer from './reducers'
-import logger from './middlewares/logger'
-import synker from './middlewares/synker'
 
 export default createStore(
-  rootReducer,
-  applyMiddleware(logger, synker)
+  rootReducer
 )
 
